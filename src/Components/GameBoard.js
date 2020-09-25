1d9831b3154dab75c916b7aa9a0a0d36b8ce0204
Trevor Thompson <Thompson_Trevor@bah.com>
2020-09-24 21:24:21 -0400

    Changed component code

--- src/Components/GameBoard.js
+++ src/Components/GameBoard.js
@@ -7,17 +7,6 @@ const CELL_SIZE = 20;
 const WIDTH = 800;
 const HEIGHT = 600;
 
-// const Cell = (props) => {
-
-//     const { x, y } = props;
-
-//     return (
-//         <div className="Cell" style={{ left: `${CELL_SIZE * x + 1}px}`, top: `${CELL_SIZE * y + 1} px`, width: `${CELL_SIZE - 1}px`, height: `${CELL_SIZE - 1}px`, }}>
-
-//         </div>
-//     )
-// }
-
 const GameBoard = () => {
 
     let rows = HEIGHT / CELL_SIZE;
